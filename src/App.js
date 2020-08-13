c6c7a54408002c5ddbe3046423a24ea9f767c6b5
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-08-14 00:45:23 +0900

    UPDATE : Main Screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import logo from '../public/image/logo.png';
 import './App.css';
 
+class CircleTitle extends Component {
+  render() {
+    return(
+      <>
+        <p className="subTitle">디자이너 모여라! 다 모여!</p>
+        <img src="/images/logo.png"></img>
+        <div className="mainLine"></div>
+        <img src="/images/DPTI.png"></img>
+        <p className="questionText">너 혹시 디자이너니? <br></br> 그럼 어떤 디자이너니?</p>
+      </>
+    );
+  }
+}
+
+class Infomation extends Component {
+  render() {
+    return (
+      <>
+        <p className="infoText">
+        디자인 성향 검사 <br/>
+        <span className="infoHighlight">DPTI (Design Pattern Type Indicator)</span>는 <br/>
+        자신의 디자인 성향을 파악할 수 있는 테스트입니다. <br/>
+        총 <span className="infoHighlight">20문항</span>으로 이루어져 있으며 <br/>
+        약 <span className="infoHighlight">3분</span>의 시간이 소요됩니다. <br/>
+        <br/>
+        <span className="infoHighlight">질문을 깊게 생각하지 마시고 직관적으로 선택해주세요!</span> </p>
+        <div className="mainLine" id="longLine"></div>
+      </>
+    );
+  }
+}
+
+class MainScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {page : 0};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  state = {
+    page : 0
+  }
+
+  handleClick = () => {
+    this.setState({
+      page : this.state.page + 1
+    });
+  }
+
+  render() {
+    return(
+    <>
+      <div className="main">
+        <div className="mainCenter">
+          <div className="dptiMainCircle">
+            <div id="center">
+              {this.state.page === 0 ? <CircleTitle></CircleTitle> : <Infomation></Infomation>}          
+            </div>
+          </div>
+          
+          <div className={this.state.page === 0 ? "participantBox" : "participantBoxHidden"}>
+            <span className="participantTitle">현재까지 응시자 수</span>
+            <span className="participantNum">159,753명</span>
+          </div>
+
+          <input type="button" className="startBox" onClick={ function(ev) {
+            this.handleClick();
+            console.log(this.props.page);
+            
+          }.bind(this)} value={this.state.page === 0 ? "DPTI START" : "알겠어요!"}/>
+          <p className="mainCopyright">copyright 2020. SJS all right reserved</p>
+        </div>
+      </div>
+    </>
+    );
+  }
+}
+
 function App() {
+  var currentPage = 0;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <MainScreen page={currentPage}></MainScreen>
+    </>
   );
 }
 
