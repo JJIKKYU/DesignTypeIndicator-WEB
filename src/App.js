2a906754eb0eedd8b7a0057759ef0ec76c312cd2
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-08-15 16:53:10 +0900

    UPDATE : button click -> move card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import logo from '../public/image/logo.png';
 import './App.css';
+import { render } from '@testing-library/react';
+
+window.page = 0;
 
 class CircleTitle extends Component {
   render() {
@@ -45,13 +48,26 @@ class MainScreen extends Component {
   }
 
   state = {
-    page : 0
+    page : 0,
+    buttonText : "DPTI START"
   }
 
   handleClick = () => {
-    this.setState({
-      page : this.state.page + 1
-    });
+    console.log(this.state.page);
+    if (this.state.page == 1) {
+      this.handleClickTest();
+    }
+    else
+    {
+      this.setState({
+        page : this.state.page + 1
+      });
+    }
+    
+  }
+
+  handleClickTest = () => {
+    this.props.onSubmit(100);
   }
 
   render() {
@@ -70,11 +86,10 @@ class MainScreen extends Component {
             <span className="participantNum">159,753명</span>
           </div>
 
-          <input type="button" className="startBox" onClick={ function(ev) {
+          <form onSubmit={this.handleClickTest}><input type="button" className="startBox" onClick={ function(ev) {
             this.handleClick();
-            console.log(this.props.page);
             
-          }.bind(this)} value={this.state.page === 0 ? "DPTI START" : "알겠어요!"}/>
+          }.bind(this)} value={this.state.page === 0 ? "DPTI START" : "알겠어요!"}/></form>
           <p className="mainCopyright">copyright 2020. SJS all right reserved</p>
         </div>
       </div>
@@ -88,27 +103,19 @@ class SurveySelectBox extends Component {
   {
     super(props);
     this.state.button = this.props.button
-    this.handleClick = this.handleClick.bind(this);
+    this.state.type = this.props.type
   }
 
   state = {
+    type : 'A',
     button : 0
   }
-
-  handleClick = () => {
-    this.setState(
-      {
-        button : this.props.button
-      }
-    );
-  }
-
   render() {
     return(
       <>
       <input type="Button" className="selectBox" value={this.props.text} onClick={function()
       {
-        console.log(this.state.button);
+        console.log(this.state.type + this.state.button);
       }.bind(this)}>
       </input>
       </>
@@ -117,6 +124,27 @@ class SurveySelectBox extends Component {
 }
 
 class SurveyCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state.number = this.props.number;
+    console.log("초기화가 되었습니다 : " + this.state.number);
+  }
+
+  state = {
+    number : 0
+  }
+
+  checkType = () => {
+    if (this.state.number < 5)
+      return 'A';
+    else if (this.state.number < 10)
+      return 'B';
+    else if (this.state.number < 15)
+      return 'C';
+    else if (this.state.number < 20)
+      return 'D';
+  }
+
   render() {
     const answerList = [
       "맞아요 매우 그래요",
@@ -125,16 +153,15 @@ class SurveyCard extends Component {
       "아닌것 같아요",
       "아니요 저는 안그래요"
     ]
+
+    const answers = answerList.map((answerText, index) => (<SurveySelectBox text={answerText} button={index} type={this.checkType()}></SurveySelectBox>));
+
     return(
       <>
       <div className="surveyBox">
-        <h1>Q{this.props.number}.</h1>
+        <h1>Q{this.state.number}.</h1>
           <p>{this.props.title}</p>
-          <SurveySelectBox text="맞아요 매우 그래요" button="0"></SurveySelectBox>
-          <SurveySelectBox text="맞는것 같아요" button="1"></SurveySelectBox>
-          <SurveySelectBox text="그냥 그래요" button="2"></SurveySelectBox>
-          <SurveySelectBox text="아닌것 같아요" button="3"></SurveySelectBox>
-          <SurveySelectBox text="아니요 저는 안그래요" button="4"></SurveySelectBox>
+          {answers}
           
         </div>
       </>
@@ -143,44 +170,107 @@ class SurveyCard extends Component {
 }
 
 class Survey extends Component {
+
+  constructor(props) {
+    super(props);
+    // this.state.currentXpos = -225;
+    // this.state.xPosTransitionStep = 450;
+
+    this.nextSurveyCard = this.nextSurveyCard.bind(this);
+  }
+
+  state = {
+    currentXpos : -226,
+    xPosTransitionStep : 454,
+  }
+
+  nextSurveyCard = () => { 
+    const finalXpos = this.state.currentXpos - this.state.xPosTransitionStep;
+
+    this.setState ({
+      currentXpos : this.state.currentXpos - this.state.xPosTransitionStep
+    });
+    console.log(this.state.currentXpos);
+  }
+
   render() {
     const questionList = [
+      "의견을 결정해야 할 때 너의 의견을 어필하는 편이니?",
+      "의견을 결정해야 할 때 너의 의견을 어필하는 편이니?",
       "의견을 결정해야 할 때 너의 의견을 어필하는 편이니?",
       "의견을 결정해야 할 때 너의 의견을 어필하는 편이니?",
       "의견을 결정해야 할 때 너의 의견을 어필하는 편이니?",
     ]
     const question = questionList.map((questionText, index) => (<SurveyCard key={index + 1} number={index + 1} title={questionText}></SurveyCard>));
 
+    // let step = 450;
+    // let currentXpos = -225;  
+    let test = {
+      transform : "translate(" + this.state.currentXpos + "px)"
+    }
+
     return(
       <>
-
-      
       <div className="main">
-        <div className="mainCenter" id="surveyMainCenter">
-          {question}
-          {/* <SurveyCard number="1" title="의견을 결정해야 할 때 너의 의견을 어필하는 편이니?"></SurveyCard>
-          <SurveyCard number="2" title="혼자만의 시간을 가지면서 에너지를 충전하는 편이니?"></SurveyCard>
-          <SurveyCard number="2" title="혼자만의 시간을 가지면서 에너지를 충전하는 편이니?"></SurveyCard>
-          <SurveyCard number="2" title="혼자만의 시간을 가지면서 에너지를 충전하는 편이니?"></SurveyCard>
-          <SurveyCard number="2" title="혼자만의 시간을 가지면서 에너지를 충전하는 편이니?"></SurveyCard>
-          <SurveyCard number="2" title="혼자만의 시간을 가지면서 에너지를 충전하는 편이니?"></SurveyCard>
-          <SurveyCard number="2" title="혼자만의 시간을 가지면서 에너지를 충전하는 편이니?"></SurveyCard> */}
+        <div className="surveyHeader">
+          <img className="headerLogo" src="/images/logo.png" alt=""/>
+          <p>2 / 20</p>
+          <img className="xButton" src="/images/X.png" alt=""/>
+          <div className="progressBarBG">
+            <div className="progressBar"></div>
+          </div>
         </div>
+        <div style={test} id="surveyMainCenter">
+          {question}
+        </div>
+        <div className="buttonDiv">
+          <input type="button" className="nextBox" value="다음으로" onClick={this.nextSurveyCard}></input>
+        </div>
+        
       </div>
       </>
     );
   }
 }
 
-function App() {
-  var currentPage = 0;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {currentPage : 0};
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+  }
+  
+  state = {
+    currentPage : 0
+  }
 
-  return (
-    <>
-    {/* <MainScreen page={currentPage}></MainScreen> */}
-    <Survey></Survey>
-    </>
-  );
+  onSearchSubmit(props) {
+    this.setState({
+      currentPage : props
+    });
+  }
+
+  screenRender() {
+    if (this.state.currentPage < 1)
+    {
+      return <MainScreen page={this.state.currentPage} onSubmit={this.onSearchSubmit} ></MainScreen>;
+    }
+    else if (this.state.currentPage >= 1)
+    {
+      return <Survey></Survey>;
+    }
+  }
+
+  render()
+  {
+    return (
+      <>
+      {/* {this.screenRender()} */}
+      {/* <MainScreen page={this.state.currentPage} onSubmit={this.onSearchSubmit} ></MainScreen> */}
+      <Survey></Survey>
+      </>
+    );
+  }
 }
 
 export default App;
